57e3bb05dbddba493572e5fa3100779b2c778a9f
Gatoreno <erp.reno@hotmail.com>
2020-10-26 18:18:42 -0400

    adding adroid

---
+++ NofityTest.Android/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Common;
+using Xamarin.Essentials;
+
+namespace NofityTest.Droid
+{
+    [Activity(Label = "NofityTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            IsPlayServicesAvailable();
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+
+
+        public void IsPlayServicesAvailable()
+        {            
+            string msgText = "";
+            bool isGooglePlayServce;
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    msgText = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                else
+                {
+                    msgText = "This device is not supported";
+                    Finish();
+                }
+                isGooglePlayServce= false;
+            }
+            else
+            {
+                msgText = "Google Play Services is available.";
+                isGooglePlayServce= true;
+            }
+            Preferences.Set("isGooglePlayServce", isGooglePlayServce);
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
